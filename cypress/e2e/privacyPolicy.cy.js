eacd6749252ab0ee3a7ef7cf9025064c2b8c8117
Murilo Frederico Teixeira <murilot@me.local>
2025-02-20 10:48:10 -0300

    Success testing

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -2,6 +2,6 @@ it.only('Testa a página de política de privacidade de forma independente', () 
     cy.visit('./src/privacy.html')
 
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
-    cy.contains('p', 'No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensinooo.').should('be.visible')
+    cy.contains('p', 'No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensino.').should('be.visible')
     cy.contains('p', 'Talking About Testing').should('be.visible')
   })
