171e4d8a63e14618f6a386650bea94a607979ecf
Murilo Frederico Teixeira <murilot@me.local>
2025-02-21 13:49:25 -0300

    New commands tests 21_02_2025

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,7 +1,9 @@
-it('Testa a página de política de privacidade de forma independente', () => {
+Cypress._.times(3, () =>{
+  it('Testa a página de política de privacidade de forma independente', () => {
     cy.visit('./src/privacy.html')
 
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
     cy.contains('p', 'No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensino.').should('be.visible')
-    cy.contains('p', 'Talking About Testing').should('be.visible')
+    cy.contains('p', 'Talking About Testing').should('be.vissible')
   })
+})
