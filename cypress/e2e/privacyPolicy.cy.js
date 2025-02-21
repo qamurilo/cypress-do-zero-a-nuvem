cf8df3078c31254972a99462110dd52420fd84ae
Murilo Frederico Teixeira <murilot@me.local>
2025-02-21 13:57:38 -0300

    Fix the last error, new version 21_02_2025

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -4,6 +4,6 @@ Cypress._.times(3, () =>{
 
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
     cy.contains('p', 'No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensino.').should('be.visible')
-    cy.contains('p', 'Talking About Testing').should('be.vissible')
+    cy.contains('p', 'Talking About Testing').should('be.visible')
   })
 })
